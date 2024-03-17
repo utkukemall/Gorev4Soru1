3f7997312ab1136a12d2ae8f77037257ab60323f
utkukemall <utkukemal1999@gmail.com>
2024-03-17 14:03:07 +0300

    js düzenlemeleri

--- dashboard.js
+++ dashboard.js
@@ -1,54 +1,67 @@
 document.addEventListener('DOMContentLoaded', function() {
   var data = {
     labels: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz"],
-    datasets: [{
-        label: "Kazanç",
+    datasets: [
+      {
+        label: "Kazanılan",
         data: [10, 30, 80, 43, 42, 10, 45],
-        backgroundColor: 'rgba(255, 165, 0, 0.5)',
+        data: [10, 30, 80, 43, 42, 10, 45],
+        backgroundColor: 'rgba(255, 165, 0, 0.5)', // Orange
         borderColor: 'rgba(255, 165, 0, 0.5)',
       },
       {
-        label: "Kayıp",
+        label: "Kaybedilen",
         data: [-10, -25, -17, -8, -10, -15, -45],
-        backgroundColor: '#f44336', // Material Design "Red"
+        backgroundColor: '#f44336', // Kırmızı
         borderColor: '#f44336',
-      }
-    ]
+      },
+    ],
   };
 
-  // Grafik ayarları
+  // Grafik seçenekleri
   var options = {
     scales: {
       y: {
-        beginAtZero: true
-      }
+        beginAtZero: true,
+        ticks: {
+          callback: function(value, index, values) {
+            return value + "k";
+          },
+        },
+      },
     },
     plugins: {
       tooltip: {
         enabled: true,
         callbacks: {
           label: function(context) {
-            var label = context.dataset.label || '';
+            var label = context.dataset.label || "";
             if (label) {
-              label += ': ';
+              label += ": ";
             }
             if (context.parsed.y !== null) {
               label += new Intl.NumberFormat().format(context.parsed.y);
             }
             return label;
-          }
-        }
-      }
-    }
+          },
+        },
+      },
+    },
+    legend: {
+      display: false,
+    },
+    elements: {
+      bar: {
+        borderRadius: 5,
+      },
+    },
   };
 
-  // Grafik boyutlarını ayarlama
-  var ctx = document.getElementById('myChart').getContext('2d');
+  // Grafik oluşturma
+  var ctx = document.getElementById("myChart").getContext("2d");
   var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: "bar",
     data: data,
     options: options,
-    width: 400,
-    height: 250
   });
 });
